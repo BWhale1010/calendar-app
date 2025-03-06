cf732705e22850cb030331c78deda2e14e153de9
BWhale1010 <bawool1010@daum.net>
2025-03-06 15:12:23 +0900

    캘린더 앱 기능 완성(미팅, todo, 방해금지, 외출)

--- src/main/java/com/example/calendarapp/CalendarAppApplication.java
+++ src/main/java/com/example/calendarapp/CalendarAppApplication.java
@@ -1,13 +1,50 @@
 package com.example.calendarapp;
 
+import com.example.calendarapp.event.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class CalendarAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CalendarAppApplication.class, args);
+        Schedule schedule = new Schedule();
+
+        HashSet<String> participants = new HashSet<String>();
+        participants.add("paul.jo");
+
+        Meeting meeting1 = new Meeting(
+                1, "meeting1",
+                ZonedDateTime.now(), ZonedDateTime.now().plusHours(1),
+                participants, "meetingRoomA", "스터디"
+        );
+
+        schedule.add(meeting1);
+
+        Todo todo1 = new Todo(
+                2, "todo1",
+                ZonedDateTime.now().plusHours(3), ZonedDateTime.now().plusHours(4),
+                "할일 적기"
+        );
+
+        schedule.add(todo1);
+
+        Todo todo2 = new Todo(
+                3, "todo2",
+                ZonedDateTime.now().plusHours(5), ZonedDateTime.now().plusHours(4),
+                "할일 적기"
+        );
+
+        schedule.add(todo1);
+
+        schedule.printAll();
+
     }
 
 }
